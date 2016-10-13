ded8b40e837bfbc88eed7c3869e4e47b2c15f9b5
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2016-10-12 20:07:35 -0500

    more config for hal1

--- system_prop.mk
+++ system_prop.mk
@@ -39,7 +39,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1 \
-    media.stagefright.legacyencoder=true
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
