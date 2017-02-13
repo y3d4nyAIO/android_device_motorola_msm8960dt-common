d8179ff02acd8b2aff47e6a5a819bcb9825461aa
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-02-12 21:13:45 -0600

    system_prop: add more camera related
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -40,6 +40,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1 \
+    persist.camera.HAL3.enabled=0 \
     media.stagefright.legacyencoder=true \
     media.stagefright.less-secure=true
 
